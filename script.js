e5bd637d7afe71835562a7597be6b3bf28402a4a
Naveen <ramnkartik@gmail.com>
2024-01-12 07:27:04 +0530

    Header and Main Calendar completed

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const date = new Date();
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
-const renderCalendar = () => {
+const getDates = () => {
   date.setDate(1);
-
-  const monthDays = document.querySelector(".days");
 
   const lastDay = new Date(
     date.getFullYear(),
@@ -25,49 +38,56 @@ const renderCalendar = () => {
     0
   ).getDay();
 
-  const nextDays = 7 - lastDayIndex - 1;
+  const nextDay = 7 - lastDayIndex - 1;
 
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  // document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-
-  document.querySelector(".date p").innerHTML = months[date.getMonth()] + " " + date.getFullYear();
-
-  let days = "";
+  let days = [];
 
   for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+    days.push(`<div class="prev-date">${prevLastDay - x + 1}</div>`);
   }
 
   for (let i = 1; i <= lastDay; i++) {
     if (
       i === new Date().getDate() &&
-      date.getMonth() === new Date().getMonth()
+      date.getMonth() === new Date().getMonth() &&
+      date.getFullYear() === new Date().getFullYear()
     ) {
-      days += `<div class="today">${i}</div>`;
+      days.push(`<div class="today">${i}</div>`);
     } else {
-      days += `<div>${i}</div>`;
+      days.push(`<div class="date">${i}</div>`);
     }
   }
 
-  for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="next-date">${j}</div>`;
-    monthDays.innerHTML = days;
+  for (let j = 1; j <= nextDay; j++) {
+    days.push(`<div class="next-date">${j}</div>`);
   }
+
+  return days;
 };
+
+const renderCalendar = () => {
+  const monthDays = document.querySelector(".calendar");
+  document.querySelector(".date p").innerHTML = months[date.getMonth()] + " " + date.getFullYear();
+  
+  document.querySelector(".calendar").style.gridTemplateRows = `repeat(${monthDays%7}, 1fr)`;
+
+  const days = getDates();
+  let daysElement = "";
+
+  for (let i = 0; i < 7; i++) {
+    daysElement += `<div class="date-box">${daysOfWeek[i] + days[i]}</div>`;
+  }
+
+  for (let i = 7; i < days.length; i++) {
+    daysElement += `<div class="date-box">${days[i]}</div>`;
+  }
+
+  monthDays.innerHTML = daysElement;
+}
+
+const renderMiniCalendar = () => {
+  
+}
 
 document.querySelector(".prev").addEventListener("click", () => {
   date.setMonth(date.getMonth() - 1);
@@ -80,7 +100,8 @@ document.querySelector(".next").addEventListener("click", () => {
 });
 
 document.querySelector(".today-button").addEventListener("click", () => {
-  date.setMonth(new Date().getMonth());
+  const currDate = new Date();
+  date.setFullYear(currDate.getFullYear(), currDate.getMonth(), currDate.getDate());
   renderCalendar();
 });
 
