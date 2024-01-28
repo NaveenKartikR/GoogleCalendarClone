7291b71f5d39a1ffbd83e962020e2cd1b41b2223
Naveen <ramnkartik@gmail.com>
2024-01-28 20:36:15 +0530

    Added Task window html, css

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const months = [
 ];
 const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
+let dates, days, extraDays;
+
 const getDates = (dateInput) => {
   dateInput.setDate(1);
 
@@ -42,9 +44,30 @@ const getDates = (dateInput) => {
   const nextDay = 7 - lastDayIndex - 1;
 
   let days = [];
+  let dates = [];
+  let currMY = [dateInput.getMonth() + 1, dateInput.getFullYear()];
+  let prevMY = [], nextMY = [];
+  if (currMY[0] == 1) {
+    prevMY[0] = 12;
+    prevMY[1] = currMY[1]-1;
+  }
+  else {
+    prevMY[0] = currMY[0]-1;
+    prevMY[1] = currMY[1];
+  }
+
+  if (currMY[0] == 12) {
+    nextMY[0] = 1;
+    nextMY[1] = currMY[1]+1;
+  }
+  else {
+    nextMY[0] = currMY[0]+1;
+    nextMY[1] = currMY[1];
+  }
 
   for (let x = firstDayIndex; x > 0; x--) {
     days.push(`<div class="prev-date">${prevLastDay - x + 1}</div>`);
+    dates.push(`d${prevLastDay - x + 1}-${prevMY[0]}-${prevMY[1]}`);
   }
 
   for (let i = 1; i <= lastDay; i++) {
@@ -54,13 +77,16 @@ const getDates = (dateInput) => {
       dateInput.getFullYear() === new Date().getFullYear()
     ) {
       days.push(`<div class="today">${i}</div>`);
+      dates.push("today-date");
     } else {
       days.push(`<div class="date">${i}</div>`);
+      dates.push(`d${i}-${currMY[0]}-${currMY[1]}`);
     }
   }
 
   for (let j = 1; j <= nextDay; j++) {
     days.push(`<div class="next-date">${j}</div>`);
+    dates.push(`d${j}-${nextMY[0]}-${nextMY[1]}`);
   }
 
   let extraDays = [];
@@ -69,26 +95,37 @@ const getDates = (dateInput) => {
       extraDays.push(`<div class="next-date">${j}</div>`);
     }
   }
-  return [days, extraDays];
+  return [dates, days, extraDays];
 };
 
 const renderCalendar = () => {
   const monthDays = document.querySelector(".calendar");
   document.querySelector(".date p").innerHTML = months[date.getMonth()] + " " + date.getFullYear();
 
-  let [days, extraDays] = getDates(date);
+  [dates, days, extraDays] = getDates(date);
   document.querySelector(".calendar").style.gridTemplateRows = `repeat(${days.length/7}, 1fr)`;
   let daysElement = "";
 
   for (let i = 0; i < 7; i++) {
-    daysElement += `<div class="date-box">${daysOfWeek[i] + days[i]}</div>`;
+    daysElement += `<div class="date-box ${dates[i]}">${daysOfWeek[i] + days[i]}</div>`;
   }
 
   for (let i = 7; i < days.length; i++) {
-    daysElement += `<div class="date-box">${days[i]}</div>`;
+    daysElement += `<div class="date-box ${dates[i]}">${days[i]}</div>`;
   }
 
   monthDays.innerHTML = daysElement;
+
+  // Add event listener each date-box rendered in current month
+  const dateBoxes = document.querySelectorAll(".date-box");
+
+  dateBoxes.forEach((dateBox) => {
+    dateBox.addEventListener("click", () => {
+      let classList = dateBox.classList;
+      openTaskWindow(classList[1]);
+    });
+  });
+
   miniDate.setFullYear(date.getFullYear(), date.getMonth(), date.getDate());
   renderMiniCalendar();
 
@@ -98,7 +135,7 @@ const renderMiniCalendar = () => {
   const monthDays = document.querySelector(".mini-calendar");
   document.querySelector(".curr-date").innerHTML = months[miniDate.getMonth()] + " " + miniDate.getFullYear();
 
-  let [days, extraDays] = getDates(miniDate);
+  let [dates, days, extraDays] = getDates(miniDate);
   if (days.length/7 !== 6) {
     days = days.concat(extraDays);
   };
@@ -155,6 +192,65 @@ document.getElementById("togC").addEventListener("click", () => {
     arrow.classList.remove("fa-chevron-up");
     arrow.classList.add("fa-chevron-down");
   }
+});
+
+let dateBoxValue = "";
+
+function openTaskWindow(date) {
+  const taskWindow = document.querySelector(".task-window");
+  const width = 30, height = 60;
+  taskWindow.style.width = width+"vw";
+  taskWindow.style.height = height+"vh";
+
+  const dateIndex = dates.findIndex((element) => { 
+    // console.log(`'${element}'`, `'${date}'`, element==date);
+    return element == date; 
+  });
+
+  const [row, col] = [Math.trunc(dateIndex/7), dateIndex%7];
+  let leftVal = 18 + 82/7*(col+1);
+  let topVal = 10 + 90/10;
+  
+  if (leftVal+width < 100) { 
+    taskWindow.style.left = leftVal + "vw"; 
+  }
+  else {
+    taskWindow.style.left = 100-(82/7*(7-col))-width + "vw";
+  }
+  taskWindow.style.top = topVal + "vh";
+
+  if (taskWindow.style.display === "none") {
+    taskWindow.style.display = "flex";
+  }
+  else {
+    taskWindow.style.display = "none";
+  }
+  dateBoxValue = date;
+}
+
+function addTask(taskTitle) {
+  const dateBox = document.querySelector(`.${dateBoxValue}`);
+  const taskNode = document.createElement("div");
+  taskNode.innerHTML = taskTitle;
+  taskNode.style.width = "calc(70vw/7)";
+  taskNode.style.height = "20px";
+  taskNode.style.color = "white";
+  taskNode.style.fontWeight = "500";
+  taskNode.style.fontSize = "13px";
+  taskNode.style.textAlign = "left";
+  taskNode.style.backgroundColor = "#1967D2";
+  taskNode.style.borderRadius = "2px";
+  taskNode.style.padding = "0px";
+  taskNode.style.margin = "5px 0px 0px 0px";
+  dateBox.appendChild(taskNode);
+}
+
+document.querySelector(".add-task").addEventListener("click", () => {
+  const titleTextNode = document.querySelector(".title-text");
+  const title = titleTextNode.value;
+  titleTextNode.value = "";
+  addTask(title);
+  document.querySelector(".task-window").style.display = "none";
 });
 
 renderCalendar();
